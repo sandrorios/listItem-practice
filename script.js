68480af8df5c9bbec5bf405aba654f161b7e76de
sandrorios <sandroderios@hotmail.com>
2023-11-09 16:42:13 -0500

    added some code to script.js

---
+++ script.js
@@ -0,0 +1,17 @@
+let formItem = document.getElementById('form-item');
+let listItem = document.getElementById('list-item');
+let inputField = document.getElementById('input-field');
+let btn = document.querySelector('btn');
+let filter = document.getElementById('filter');
+let clearBtn = document.getElementById('clear');
+
+function addItem(e){
+    e.preventDefault();
+    let newItem = inputField.value;
+    if(newItem === ''){
+        alert("Please add an Item");
+        return;
+    }
+}
+
+formItem.addEventListener('submit', addItem);
