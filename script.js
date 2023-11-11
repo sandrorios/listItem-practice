ce56028f5c9ac771eba61d7d46509aa9203d5f4c
sandrorios <sandroderios@hotmail.com>
2023-11-10 21:48:43 -0500

    code to be edited

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ let listItem = document.getElementById('list-item');
 let inputField = document.getElementById('input-field');
 let btn = document.querySelector('btn');
 let filter = document.getElementById('filter');
-let clearBtn = document.getElementById('filter');
+let clearBtn = document.getElementById('clear');
 
 function addItem(e){
     e.preventDefault();
